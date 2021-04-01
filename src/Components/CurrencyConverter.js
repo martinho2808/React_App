62a13a26d42404c2d69af176ffd42c4eeeed5551
Sam O'Shaughnessy <samoshaughnessy123@hotmail.com>
2021-04-01 16:29:56 +0800

    adding hooks currency converter

--- src/Components/CurrencyConverter.js
+++ src/Components/CurrencyConverter.js
@@ -69,3 +69,63 @@ export default class CurrencyConverter extends React.Component {
     );
   }
 }
+
+// Functional Hook based Currency Converter
+
+// import { useState } from "react";
+
+// export default function CurrencyConverter() {
+//   const [currencies, setCurrencies] = useState({
+//     USD: 1,
+//     HKD: 7.77,
+//     GBP: 0.73,
+//   });
+
+//   return (
+//     <>
+//       <h4>Currency Converter</h4>
+//       <label>HKD:</label>
+//       <input
+//         name="hkd"
+//         value={currencies.HKD}
+//         onChange={(e) =>
+//           setCurrencies({
+//             HKD: e.currentTarget.value,
+//             USD: e.currentTarget.value * 0.13,
+//             GBP: e.currentTarget.value * 0.093,
+//           })
+//         }
+//       />
+//       <br />
+//       <br />
+//       <label>USD:</label>
+//       <input
+//         name="usd"
+//         value={currencies.USD}
+//         onChange={(e) =>
+//           setCurrencies({
+//             HKD: e.currentTarget.value * 7.77,
+//             USD: e.currentTarget.value,
+//             GBP: e.currentTarget.value * 0.73,
+//           })
+//         }
+//       />
+//       <br />
+//       <br />
+//       <label>GBP:</label>
+//       <input
+//         name="gbp"
+//         value={currencies.GBP}
+//         onChange={(e) =>
+//           setCurrencies({
+//             HKD: e.currentTarget.value * 10.71,
+//             USD: e.currentTarget.value * 1.138,
+//             GBP: e.currentTarget.value,
+//           })
+//         }
+//       />
+//       <br />
+//       <br />
+//     </>
+//   );
+// }
