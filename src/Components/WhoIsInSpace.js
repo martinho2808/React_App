9fb10243234788ea51053d5df194c9df606402f2
Sam O'Shaughnessy <samoshaughnessy123@hotmail.com>
2021-04-08 16:24:41 +0800

    who is in space

--- src/Components/WhoIsInSpace.js
+++ src/Components/WhoIsInSpace.js
@@ -1,60 +1,60 @@
-// Class based WhoIsInSpace- LCM
+// // Class based WhoIsInSpace- LCM
 
-import React from "react";
-export default class GetSpaceMan extends React.Component {
-  constructor(props) {
-    super(props);
+// import React from "react";
+// export default class GetSpaceMan extends React.Component {
+//   constructor(props) {
+//     super(props);
 
-    this.state = {
-      people: [],
-    };
-  }
+//     this.state = {
+//       people: [],
+//     };
+//   }
 
-  componentDidMount() {
-    fetch(`http://api.open-notify.org/astros.json`)
-      .then((res) => res.json())
-      .then(
-        (result) => {
-          console.log(result);
-          this.setState({
-            people: [...result.people],
-          });
-        },
-        (error) => {
-          console.log(error);
-        }
-      );
-  }
+//   componentDidMount() {
+//     fetch(`http://api.open-notify.org/astros.json`)
+//       .then((res) => res.json())
+//       .then(
+//         (result) => {
+//           console.log(result);
+//           this.setState({
+//             people: [...result.people],
+//           });
+//         },
+//         (error) => {
+//           console.log(error);
+//         }
+//       );
+//   }
 
-  render() {
-    const people =
-      this.state.people && this.state.people.length > 0 ? (
-        this.state.people.map((person, i) => {
-          return (
-            <div key={i}>
-              {" "}
-              <p>{person.name}</p>
-            </div>
-          );
-        })
-      ) : (
-        <div>
-          <p>No space men?</p>
-        </div>
-      );
+//   render() {
+//     const people =
+//       this.state.people && this.state.people.length > 0 ? (
+//         this.state.people.map((person, i) => {
+//           return (
+//             <div key={i}>
+//               {" "}
+//               <p>{person.name}</p>
+//             </div>
+//           );
+//         })
+//       ) : (
+//         <div>
+//           <p>No space men?</p>
+//         </div>
+//       );
 
-    return (
-      <>
-        <div>
-          <br />
-          <br />
-          <h3>Already have spacemen</h3>
-          {people}
-        </div>
-      </>
-    );
-  }
-}
+//     return (
+//       <>
+//         <div>
+//           <br />
+//           <br />
+//           <h3>Already have spacemen</h3>
+//           {people}
+//         </div>
+//       </>
+//     );
+//   }
+// }
 
 // Component Did Update
 
@@ -143,3 +143,55 @@ export default class GetSpaceMan extends React.Component {
 //     );
 //   }
 // }
+
+import { useState, useEffect } from "react";
+
+export default function GetSpaceMan(props) {
+  const [spacemen, setSpacemen] = useState([]);
+
+  useEffect(() => {
+    // Component Did Mount
+    fetch(`http://api.open-notify.org/astros.json`)
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          console.log(result);
+          setSpacemen([...result.people]);
+          console.log(spacemen);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    return () => {
+      // Component Did Unmount
+    };
+  }, []);
+
+  const people =
+    spacemen && spacemen.length > 0 ? (
+      spacemen.map((person, i) => {
+        return (
+          <div key={i}>
+            {" "}
+            <p>{person.name}</p>
+          </div>
+        );
+      })
+    ) : (
+      <div>
+        <p>No space men?</p>
+      </div>
+    );
+
+  return (
+    <>
+      <div>
+        <br />
+        <br />
+        <h3>Find space men?</h3>
+        {people}
+      </div>
+    </>
+  );
+}
