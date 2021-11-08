3d26cdad8be27550c70e2244f7df90441638891a
Sam O'Shaughnessy <samoshaughnessy123@hotmail.com>
2021-11-08 10:05:57 +0800

    adding hooks

--- src/Components/LinkList.js
+++ src/Components/LinkList.js
@@ -1,217 +1,217 @@
-import React from "react";
-import { connect } from "react-redux";
-import {
-  CLEAR_LINKS,
-  AddLink,
-  DeleteLink,
-  loadLinkThunk,
-  LOAD_LINK_SUCCESS_ACTION,
-} from "../Redux/links/actions";
-import axios from "axios";
-
-import { store } from "../Redux/store";
-
-class PureLinkList extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      url: "Insert Link Here",
-      title: "Insert Link Title Here",
-    };
-  }
-
-  // Dynamically Add Links
-  submitLink = (e) => {
-    e.preventDefault();
-
-    const link = {
-      title: this.state.title,
-      url: this.state.url,
-    };
-
-    this.props.addLinkMDP(link);
-
-    this.setState({
-      url: "Insert Link Here",
-      title: "Insert Link Title Here",
-    });
-  };
-
-  // Function to handle input changes
-  handleInputChange = (e) => {
-    if (e.currentTarget.name === "url") {
-      this.setState({
-        url: e.currentTarget.value,
-      });
-    } else if (e.currentTarget.name === "title") {
-      this.setState({
-        title: e.currentTarget.value,
-      });
-    }
-  };
-
-  componentDidMount() {
-    // More conventional way
-    axios.get("https://www.reddit.com/r/programming.json").then((response) => {
-      let threads = response.data;
-      let redditLinks = threads.data.children.map((link) => ({
-        title: link.data.title,
-        url: link.data.url,
-      }));
-      console.log(redditLinks);
-      store.dispatch({
-        type: LOAD_LINK_SUCCESS_ACTION,
-        payload: redditLinks,
-      });
-    });
-  }
-
-  render() {
-    return (
-      <>
-        {/* Code to add links dynamically */}
-        <h3>Add Link</h3>
-
-        <input
-          type="text"
-          value={this.state.url}
-          name="url"
-          onChange={this.handleInputChange}
-        />
-        <br />
-        <input
-          type="text"
-          value={this.state.title}
-          name="title"
-          onChange={this.handleInputChange}
-        />
-        <br />
-        <button onClick={this.submitLink}>New Link</button>
-        <button onClick={this.props.clearLinkMDP}>Clear Links</button>
-        <button onClick={this.props.loadLinkThunkMDP}>Load Links</button>
-
-        {/* Code to display links from the Redux store */}
-        <h3>Links:</h3>
-        {/* {this.props.links.map((link, i) => (
-          <div key={i}>
-            {link.title} - {link.url}
-            <button onClick={() => this.props.deleteLinkMDP(i)}>
-              Delete Link
-            </button>
-          </div>
-        ))} */}
-
-        {this.props.loading
-          ? "Loading...."
-          : this.props.links.map((link, i) => (
-              <div key={i}>
-                {link.title} - {link.url}
-                <button onClick={() => this.props.deleteLinkMDP(i)}>
-                  Delete Link
-                </button>
-              </div>
-            ))}
-      </>
-    );
-  }
-}
-
-const mapStateToProps = (state) => {
-  return {
-    links: state.linkStore.links,
-    loading: state.linkStore.loading,
-  };
-};
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    // dispatching an action to alter the state
-    addLinkMDP: (link) => dispatch(AddLink(link)),
-    // dispatching an action creator
-    clearLinkMDP: () =>
-      dispatch({
-        type: CLEAR_LINKS,
-      }),
-    deleteLinkMDP: (i) => dispatch(DeleteLink(i)),
-    loadLinkThunkMDP: () => dispatch(loadLinkThunk()),
-  };
-};
-
-export const LinkList = connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(PureLinkList);
-
-// import { useState } from "react";
-// import { useDispatch, useSelector } from "react-redux";
+// import React from "react";
+// import { connect } from "react-redux";
 // import {
 //   CLEAR_LINKS,
 //   AddLink,
 //   DeleteLink,
 //   loadLinkThunk,
+//   LOAD_LINK_SUCCESS_ACTION,
 // } from "../Redux/links/actions";
+// import axios from "axios";
 
-// export const LinkList = (props) => {
-//   const [url, setUrl] = useState("Insert URL here");
-//   const [title, setTitle] = useState("Insert Title here");
+// import { store } from "../Redux/store";
 
-//   const linksFromRedux = useSelector((state) => state.linkStore.links);
+// class PureLinkList extends React.Component {
+//   constructor(props) {
+//     super(props);
 
-//   const displayLinks = linksFromRedux.map((link, i) => (
-//     <div key={i}>
-//       {link.title} - {link.url}
-//       <button
-//         onClick={() => {
-//           console.log(i);
-//           deleteLink(i);
-//         }}
-//       >
-//         Delete Link
-//       </button>
-//     </div>
-//   ));
-
-//   const dispatch = useDispatch();
-
-//   const submitLink = (e) => {
-//     e.preventDefault();
-//     const newLink = {
-//       title,
-//       url,
+//     this.state = {
+//       url: "Insert Link Here",
+//       title: "Insert Link Title Here",
 //     };
-//     dispatch(AddLink(newLink));
-//     setUrl("Insert URL here");
-//     setTitle("Insert Title here");
+//   }
+
+//   // Dynamically Add Links
+//   submitLink = (e) => {
+//     e.preventDefault();
+
+//     const link = {
+//       title: this.state.title,
+//       url: this.state.url,
+//     };
+
+//     this.props.addLinkMDP(link);
+
+//     this.setState({
+//       url: "Insert Link Here",
+//       title: "Insert Link Title Here",
+//     });
 //   };
 
-//   const clearLink = () => dispatch({ type: CLEAR_LINKS });
-//   const deleteLink = (i) => {
-//     console.log(i);
-//     dispatch(DeleteLink(i));
+//   // Function to handle input changes
+//   handleInputChange = (e) => {
+//     if (e.currentTarget.name === "url") {
+//       this.setState({
+//         url: e.currentTarget.value,
+//       });
+//     } else if (e.currentTarget.name === "title") {
+//       this.setState({
+//         title: e.currentTarget.value,
+//       });
+//     }
 //   };
 
-//   return (
-//     <>
-//       <h3>Add Link</h3>
-//       <input
-//         type="text"
-//         value={url}
-//         onChange={(e) => setUrl(e.currentTarget.value)}
-//       />
-//       <br />
-//       <input
-//         type="text"
-//         value={title}
-//         onChange={(e) => setTitle(e.currentTarget.value)}
-//       />
-//       <br />
-//       <button onClick={submitLink}> New Link</button>
-//       <button onClick={clearLink}>Clear Links</button>
-//       <button onClick={() => dispatch(loadLinkThunk())}>All Link API</button>
+//   componentDidMount() {
+//     // More conventional way
+//     axios.get("https://www.reddit.com/r/programming.json").then((response) => {
+//       let threads = response.data;
+//       let redditLinks = threads.data.children.map((link) => ({
+//         title: link.data.title,
+//         url: link.data.url,
+//       }));
+//       console.log(redditLinks);
+//       store.dispatch({
+//         type: LOAD_LINK_SUCCESS_ACTION,
+//         payload: redditLinks,
+//       });
+//     });
+//   }
 
-//       <h2>Links</h2>
-//       {displayLinks}
-//     </>
-//   );
+//   render() {
+//     return (
+//       <>
+//         {/* Code to add links dynamically */}
+//         <h3>Add Link</h3>
+
+//         <input
+//           type="text"
+//           value={this.state.url}
+//           name="url"
+//           onChange={this.handleInputChange}
+//         />
+//         <br />
+//         <input
+//           type="text"
+//           value={this.state.title}
+//           name="title"
+//           onChange={this.handleInputChange}
+//         />
+//         <br />
+//         <button onClick={this.submitLink}>New Link</button>
+//         <button onClick={this.props.clearLinkMDP}>Clear Links</button>
+//         <button onClick={this.props.loadLinkThunkMDP}>Load Links</button>
+
+//         {/* Code to display links from the Redux store */}
+//         <h3>Links:</h3>
+//         {/* {this.props.links.map((link, i) => (
+//           <div key={i}>
+//             {link.title} - {link.url}
+//             <button onClick={() => this.props.deleteLinkMDP(i)}>
+//               Delete Link
+//             </button>
+//           </div>
+//         ))} */}
+
+//         {this.props.loading
+//           ? "Loading...."
+//           : this.props.links.map((link, i) => (
+//               <div key={i}>
+//                 {link.title} - {link.url}
+//                 <button onClick={() => this.props.deleteLinkMDP(i)}>
+//                   Delete Link
+//                 </button>
+//               </div>
+//             ))}
+//       </>
+//     );
+//   }
+// }
+
+// const mapStateToProps = (state) => {
+//   return {
+//     links: state.linkStore.links,
+//     loading: state.linkStore.loading,
+//   };
 // };
+
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     // dispatching an action to alter the state
+//     addLinkMDP: (link) => dispatch(AddLink(link)),
+//     // dispatching an action creator
+//     clearLinkMDP: () =>
+//       dispatch({
+//         type: CLEAR_LINKS,
+//       }),
+//     deleteLinkMDP: (i) => dispatch(DeleteLink(i)),
+//     loadLinkThunkMDP: () => dispatch(loadLinkThunk()),
+//   };
+// };
+
+// export const LinkList = connect(
+//   mapStateToProps,
+//   mapDispatchToProps
+// )(PureLinkList);
+
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  CLEAR_LINKS,
+  AddLink,
+  DeleteLink,
+  loadLinkThunk,
+} from "../Redux/links/actions";
+
+export const LinkList = (props) => {
+  const [url, setUrl] = useState("Insert URL here");
+  const [title, setTitle] = useState("Insert Title here");
+
+  const linksFromRedux = useSelector((state) => state.linkStore.links);
+
+  const displayLinks = linksFromRedux.map((link, i) => (
+    <div key={i}>
+      {link.title} - {link.url}
+      <button
+        onClick={() => {
+          console.log(i);
+          deleteLink(i);
+        }}
+      >
+        Delete Link
+      </button>
+    </div>
+  ));
+
+  const dispatch = useDispatch();
+
+  const submitLink = (e) => {
+    e.preventDefault();
+    const newLink = {
+      title,
+      url,
+    };
+    dispatch(AddLink(newLink));
+    setUrl("Insert URL here");
+    setTitle("Insert Title here");
+  };
+
+  const clearLink = () => dispatch({ type: CLEAR_LINKS });
+  const deleteLink = (i) => {
+    console.log(i);
+    dispatch(DeleteLink(i));
+  };
+
+  return (
+    <>
+      <h3>Add Link</h3>
+      <input
+        type="text"
+        value={url}
+        onChange={(e) => setUrl(e.currentTarget.value)}
+      />
+      <br />
+      <input
+        type="text"
+        value={title}
+        onChange={(e) => setTitle(e.currentTarget.value)}
+      />
+      <br />
+      <button onClick={submitLink}> New Link</button>
+      <button onClick={clearLink}>Clear Links</button>
+      <button onClick={() => dispatch(loadLinkThunk())}>All Link API</button>
+
+      <h2>Links</h2>
+      {displayLinks}
+    </>
+  );
+};
--- src/Components/PictureList.js
+++ src/Components/PictureList.js
@@ -1,107 +1,169 @@
-import React from "react";
+// import React from "react";
+// import { connect } from "react-redux";
+// import { getImageThunk, DeleteImage } from "../Redux/pictures/actions.js";
 
-import { connect } from "react-redux";
+// class PureImageList extends React.Component {
+//   constructor(props) {
+//     super(props);
 
-import { getImageThunk, DeleteImage } from "../Redux/pictures/actions.js";
+//     this.state = {
+//       searched: "Insert Search Here",
+//       title: "Title of Searched Image",
+//     };
+//   }
 
-class PureImageList extends React.Component {
-  constructor(props) {
-    super(props);
+//   submitSearch(e) {
+//     e.preventDefault();
+//     const image = {
+//       title: this.state.title,
+//       searched: this.state.searched,
+//     };
+//     this.props.addImageMDP(image);
+//   }
 
-    this.state = {
-      searched: "Insert Search Here",
-      title: "Title of Searched Image",
-    };
-  }
+//   handleInputChange(e) {
+//     if (e.currentTarget.name === "search") {
+//       this.setState({
+//         searched: e.currentTarget.value,
+//       });
+//     } else if (e.currentTarget.name === "title") {
+//       this.setState({
+//         title: e.currentTarget.value,
+//       });
+//     }
+//   }
 
-  submitSearch(e) {
+//   render() {
+//     return (
+//       <>
+//         <h2>Search for Image</h2>
+//         <input
+//           type="text"
+//           name="title"
+//           value={this.state.title}
+//           onChange={(e) => this.handleInputChange(e)}
+//         />
+//         <br />
+//         <input
+//           type="text"
+//           name="search"
+//           value={this.state.searched}
+//           onChange={(e) => this.handleInputChange(e)}
+//         />
+//         <br />
+//         <button onClick={(e) => this.submitSearch(e)}>New Image</button>
+
+//         <h3>Images:</h3>
+//         <div
+//           style={{
+//             display: "flex",
+//             flexWrap: "wrap",
+//           }}
+//         >
+//           {this.props.images.map((image, i) => (
+//             <div key={i}>
+//               <div
+//                 style={{
+//                   textAlign: "center",
+//                 }}
+//               >
+//                 <h3>{image.title}</h3>
+//                 <img
+//                   style={{ width: "30%", height: "30%" }}
+//                   src={image.url}
+//                   alt={image.title}
+//                 />
+//                 <br />
+//                 <button onClick={() => this.props.deleteImageMDP(i)}>
+//                   Delete me
+//                 </button>
+//               </div>
+//             </div>
+//           ))}
+//         </div>
+//       </>
+//     );
+//   }
+// }
+
+// const mapStateToProps = (state) => {
+//   return {
+//     images: state.imageStore.images,
+//   };
+// };
+
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     addImageMDP: (image) => dispatch(getImageThunk(image)),
+//     deleteImageMDP: (i) => dispatch(DeleteImage(i)),
+//   };
+// };
+
+// export const PictureList = connect(
+//   mapStateToProps,
+//   mapDispatchToProps
+// )(PureImageList);
+
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getImageThunk, DeleteImage } from "../Redux/pictures/actions";
+
+export function PictureList() {
+  const [title, setTitle] = useState("Insert Title Here");
+  const [searched, setSearched] = useState("Insert Search Here");
+  const dispatch = useDispatch();
+  const storedImagesRedux = useSelector((state) => state.ImageStore.images);
+
+  function submitSearch(e) {
     e.preventDefault();
     const image = {
-      title: this.state.title,
-      searched: this.state.searched,
+      searched,
     };
-    this.props.addImageMDP(image);
+    dispatch(getImageThunk(image));
   }
 
-  handleInputChange(e) {
-    if (e.currentTarget.name === "search") {
-      this.setState({
-        searched: e.currentTarget.value,
-      });
-    } else if (e.currentTarget.name === "title") {
-      this.setState({
-        title: e.currentTarget.value,
-      });
-    }
-  }
+  return (
+    <div>
+      <h2>Search for Image</h2>
+      <input
+        type="text"
+        name="title"
+        value={title}
+        onChange={(e) => setTitle(e.currentTarget.value)}
+      />
+      <br />
+      <input
+        type="text"
+        name="search"
+        value={searched}
+        onChange={(e) => setSearched(e.currentTarget.value)}
+      />
+      <br />
+      <button onClick={(e) => submitSearch(e)}>New Image</button>
 
-  render() {
-    return (
-      <>
-        <h2>Search for Image</h2>
-        <input
-          type="text"
-          name="title"
-          value={this.state.title}
-          onChange={(e) => this.handleInputChange(e)}
-        />
-        <br />
-        <input
-          type="text"
-          name="search"
-          value={this.state.searched}
-          onChange={(e) => this.handleInputChange(e)}
-        />
-        <br />
-        <button onClick={(e) => this.submitSearch(e)}>New Image</button>
+      <br />
 
-        <h3>Images:</h3>
-        <div
-          style={{
-            display: "flex",
-            flexWrap: "wrap",
-          }}
-        >
-          {this.props.images.map((image, i) => (
-            <div key={i}>
-              <div
-                style={{
-                  textAlign: "center",
-                }}
-              >
-                <h3>{image.title}</h3>
-                <img
-                  style={{ width: "30%", height: "30%" }}
-                  src={image.url}
-                  alt={image.title}
-                />
-                <br />
-                <button onClick={() => this.props.deleteImageMDP(i)}>
-                  Delete me
-                </button>
+      <h3>Images</h3>
+      <div style={{ display: "flex", flexWrap: "wrap" }}>
+        {storedImagesRedux.length > 0
+          ? storedImagesRedux.map((image, i) => (
+              <div key={i}>
+                <div style={{ textAlign: "center" }}>
+                  <h3>{image.title}</h3>
+                  <img
+                    style={{ width: "30%", height: "30%" }}
+                    srl={image.url}
+                    alt={image.title}
+                  />
+                  <br />
+                  <button onClick={() => DeleteImage(i)}>
+                    Click to Delete
+                  </button>
+                </div>
               </div>
-            </div>
-          ))}
-        </div>
-      </>
-    );
-  }
+            ))
+          : null}
+      </div>
+    </div>
+  );
 }
-
-const mapStateToProps = (state) => {
-  return {
-    images: state.imageStore.images,
-  };
-};
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    addImageMDP: (image) => dispatch(getImageThunk(image)),
-    deleteImageMDP: (i) => dispatch(DeleteImage(i)),
-  };
-};
-
-export const PictureList = connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(PureImageList);
