9ac1c039e54f19a0eb6d419fbe6b535cf3f99526
Sam O'Shaughnessy <samoshaughnessy123@hotmail.com>
2021-11-09 12:49:31 +0800

    adding hooks

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -1,152 +1,174 @@
-import React from "react";
-import { connect } from "react-redux";
-import { loginUserThunk, loginFacebookThunk } from "../Redux/auth/actions";
-import { withRouter } from "react-router-dom";
+// import React from "react";
+// import { connect } from "react-redux";
+// import { loginUserThunk, loginFacebookThunk } from "../Redux/auth/actions";
+// import { withRouter } from "react-router-dom";
+// import FacebookLogin from "react-facebook-login";
+
+// class Login extends React.Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.state = {
+//       email: "Please insert an email here",
+//       password: "Please insert you password here",
+//     };
+//   }
+
+//   componentDidMount() {
+//     if (this.props.isAuthticated === true) {
+//       this.props.history.push("/users");
+//     }
+//   }
+
+//   login = () => {
+//     this.props.loginUserMDP(this.state.email, this.state.password);
+//   };
+
+//   handleInputChange = (e) => {
+//     if (e.currentTarget.name === "email") {
+//       this.setState({
+//         email: e.currentTarget.value,
+//       });
+//     } else if (e.currentTarget.name === "password") {
+//       this.setState({
+//         password: e.currentTarget.value,
+//       });
+//     }
+//   };
+
+//   componentClicked() {
+//     console.log("clicked");
+//     return null;
+//   }
+
+//   responseFacebook = (userInfo) => {
+//     if (userInfo.accessToken) {
+//       this.props.loginFacebookMDP(userInfo.accessToken);
+//     }
+//     return null;
+//   };
+
+//   render() {
+//     return (
+//       <>
+//         <label>Email: </label>
+//         <br />
+//         <input
+//           type="text"
+//           value={this.state.email}
+//           onChange={this.handleInputChange}
+//           name="email"
+//         />
+//         <br />
+//         <label>Password: </label>
+//         <br />
+//         <input
+//           type="text"
+//           value={this.state.password}
+//           onChange={this.handleInputChange}
+//           name="password"
+//         />
+//         <br />
+//         <button onClick={this.login}>Login </button>
+//         {this.props.isAuthticated && <p>Login works</p>}
+
+//         <FacebookLogin
+//           appId={process.env.REACT_APP_FACEBOOK_APP_ID || ""}
+//           autoLoad={true}
+//           fields="name,email,picture"
+//           onClick={this.componentClicked}
+//           callback={this.responseFacebook}
+//         />
+//       </>
+//     );
+//   }
+// }
+
+// const mapStateToProps = (state) => {
+//   return {
+//     isAuthticated: state.authStore.isAuthticated,
+//   };
+// };
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     loginUserMDP: (email, password) =>
+//       dispatch(loginUserThunk(email, password)),
+//     loginFacebookMDP: (accessToken) => {
+//       dispatch(loginFacebookThunk(accessToken));
+//     },
+//   };
+// };
+
+// export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Login));
+
+// Hooks:
 import FacebookLogin from "react-facebook-login";
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { loginUserThunk, loginFacebookThunk } from "../Redux/auth/actions";
+import { useHistory } from "react-router-dom";
 
-class Login extends React.Component {
-  constructor(props) {
-    super(props);
+const Login = (props) => {
+  const [email, setEmail] = useState("Insert Email");
+  const [password, setPassword] = useState("Insert Password");
+  const auth = useSelector((state) => state.authStore.isAuthenticated);
+  const dispatch = useDispatch();
+  const history = useHistory();
 
-    this.state = {
-      email: "Please insert an email here",
-      password: "Please insert you password here",
-    };
-  }
-
-  componentDidMount() {
-    if (this.props.isAuthticated === true) {
-      this.props.history.push("/users");
+  useEffect(() => {
+    console.log(auth);
+    if (auth === true) {
+      history.push("/users");
     }
-  }
+  }, [auth, history]);
 
-  login = () => {
-    this.props.loginUserMDP(this.state.email, this.state.password);
+  const login = () => {
+    dispatch(loginUserThunk(email, password));
   };
 
-  handleInputChange = (e) => {
-    if (e.currentTarget.name === "email") {
-      this.setState({
-        email: e.currentTarget.value,
-      });
-    } else if (e.currentTarget.name === "password") {
-      this.setState({
-        password: e.currentTarget.value,
-      });
-    }
-  };
-
-  componentClicked() {
+  function componentClicked() {
     console.log("clicked");
     return null;
   }
 
-  responseFacebook = (userInfo) => {
+  function responseFacebook(userInfo) {
     if (userInfo.accessToken) {
-      this.props.loginFacebookMDP(userInfo.accessToken);
+      loginFacebookThunk(userInfo.accessToken);
     }
     return null;
-  };
-
-  render() {
-    return (
-      <>
-        <label>Email: </label>
-        <br />
-        <input
-          type="text"
-          value={this.state.email}
-          onChange={this.handleInputChange}
-          name="email"
-        />
-        <br />
-        <label>Password: </label>
-        <br />
-        <input
-          type="text"
-          value={this.state.password}
-          onChange={this.handleInputChange}
-          name="password"
-        />
-        <br />
-        <button onClick={this.login}>Login </button>
-        {this.props.isAuthticated && <p>Login works</p>}
-
-        <FacebookLogin
-          appId={process.env.REACT_APP_FACEBOOK_APP_ID || ""}
-          autoLoad={true}
-          fields="name,email,picture"
-          onClick={this.componentClicked}
-          callback={this.responseFacebook}
-        />
-      </>
-    );
   }
-}
 
-const mapStateToProps = (state) => {
-  return {
-    isAuthticated: state.authStore.isAuthticated,
-  };
+  return (
+    <div>
+      <label>
+        Email:
+        <input
+          onChange={(e) => setEmail(e.currentTarget.value)}
+          type="text"
+          value={email}
+        />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input
+          onChange={(e) => setPassword(e.currentTarget.value)}
+          type="text"
+          value={password}
+        />
+      </label>
+      <br />
+      <button onClick={login}>Login</button>
+      {auth && <p>Login Successful!</p>}
+      <FacebookLogin
+        appId={process.env.REACT_APP_FACEBOOK_APP_ID || ""}
+        autoLoad={true}
+        fields="name,email,picture"
+        onClick={componentClicked}
+        callback={responseFacebook}
+      />
+    </div>
+  );
 };
-const mapDispatchToProps = (dispatch) => {
-  return {
-    loginUserMDP: (email, password) =>
-      dispatch(loginUserThunk(email, password)),
-    loginFacebookMDP: (accessToken) => {
-      dispatch(loginFacebookThunk(accessToken));
-    },
-  };
-};
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Login));
-
-// Hooks:
-// import React, { useState, useEffect } from "react";
-// import { useSelector, useDispatch } from "react-redux";
-// import { loginUserThunk } from "./redux/actions/actions";
-
-// const Login = (props) => {
-//   const [email, setEmail] = useState("");
-//   const [password, setPassword] = useState("");
-//   const auth = useSelector((state) => state.auth);
-//   const { isAuthenticated } = auth;
-//   const dispatch = useDispatch();
-
-//   useEffect(() => {
-//     if (isAuthenticated === true) {
-//       props.history.push("/users");
-//     }
-//   }, [isAuthenticated]);
-
-//   const login = () => {
-//     dispatch(loginUserThunk(email, password));
-//   };
-
-//   return (
-//     <div>
-//       <label>
-//         Username:
-//         <input
-//           onChange={(e) => setEmail(e.currentTarget.value)}
-//           type="text"
-//           value={email}
-//         />
-//       </label>
-//       <br />
-//       <label>
-//         Password:
-//         <input
-//           onChange={(e) => setPassword(e.currentTarget.value)}
-//           type="text"
-//           value={password}
-//         />
-//       </label>
-//       <br />
-//       <button onClick={login}>Login</button>
-//       {isAuthenticated && <p>Login Successful!</p>}
-//     </div>
-//   );
-// };
-
-// export default Login;
+export default Login;
--- src/Components/SignupForm.js
+++ src/Components/SignupForm.js
@@ -1,145 +1,130 @@
-import React from "react";
-import { connect } from "react-redux";
-import { withRouter } from "react-router-dom";
-import { signupThunk } from "../Redux/auth/actions";
+// import React from "react";
+// import { connect } from "react-redux";
+// import { withRouter } from "react-router-dom";
+// import { signupThunk } from "../Redux/auth/actions";
 
-class Signup extends React.Component {
-  constructor(props) {
-    super(props);
+// class Signup extends React.Component {
+//   constructor(props) {
+//     super(props);
 
-    this.state = {
-      email: "Please insert an email here",
-      password: "Please insert you password here",
-      name: "Please insert name here",
-    };
-  }
+//     this.state = {
+//       email: "Please insert an email here",
+//       password: "Please insert you password here",
+//       name: "Please insert name here",
+//     };
+//   }
 
-  componentDidMount() {
-    if (this.props.isAuthticated === true) {
-      this.props.history.push("/users");
-    }
-  }
-
-  signup = () => {
-    this.props.signupMDP(
-      this.state.email,
-      this.state.password,
-      this.state.name
-    );
-  };
-
-  handleInputChange = (e) => {
-    if (e.currentTarget.name === "email") {
-      this.setState({
-        email: e.currentTarget.value,
-      });
-    } else if (e.currentTarget.name === "password") {
-      this.setState({
-        password: e.currentTarget.value,
-      });
-    } else if (e.currentTarget.name === "name") {
-      this.setState({
-        name: e.currentTarget.value,
-      });
-    }
-  };
-
-  render() {
-    return (
-      <>
-        <label>Email: </label>
-        <br />
-        <input
-          type="text"
-          value={this.state.email}
-          onChange={this.handleInputChange}
-          name="email"
-        />
-        <br />
-        <label>Name: </label>
-        <br />
-        <input
-          type="text"
-          value={this.state.name}
-          onChange={this.handleInputChange}
-          name="name"
-        />
-        <br />
-        <label>Password: </label>
-        <br />
-        <input
-          type="text"
-          value={this.state.password}
-          onChange={this.handleInputChange}
-          name="password"
-        />
-        <br />
-        <button onClick={this.signup}>Signup </button>
-      </>
-    );
-  }
-}
-
-const mapStateToProps = (state) => {
-  return {
-    isAuthticated: state.authStore.isAuthticated,
-  };
-};
-const mapDispatchToProps = (dispatch) => {
-  return {
-    signupMDP: (email, password, name) =>
-      dispatch(signupThunk(email, password, name)),
-  };
-};
-
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Signup));
-
-// Hooks:
-// import React, { useState, useEffect } from "react";
-// import { useSelector, useDispatch } from "react-redux";
-// import { loginUserThunk } from "./redux/actions/actions";
-
-// const Login = (props) => {
-//   const [email, setEmail] = useState("");
-//   const [password, setPassword] = useState("");
-//   const auth = useSelector((state) => state.auth);
-//   const { isAuthenticated } = auth;
-//   const dispatch = useDispatch();
-
-//   useEffect(() => {
-//     if (isAuthenticated === true) {
-//       props.history.push("/users");
-//     }
-//   }, [isAuthenticated]);
-
-//   const login = () => {
-//     dispatch(loginUserThunk(email, password));
+//   signup = () => {
+//     this.props.signupMDP(
+//       this.state.email,
+//       this.state.password,
+//       this.state.name
+//     );
 //   };
 
-//   return (
-//     <div>
-//       <label>
-//         Username:
+//   handleInputChange = (e) => {
+//     if (e.currentTarget.name === "email") {
+//       this.setState({
+//         email: e.currentTarget.value,
+//       });
+//     } else if (e.currentTarget.name === "password") {
+//       this.setState({
+//         password: e.currentTarget.value,
+//       });
+//     } else if (e.currentTarget.name === "name") {
+//       this.setState({
+//         name: e.currentTarget.value,
+//       });
+//     }
+//   };
+
+//   render() {
+//     return (
+//       <>
+//         <label>Email: </label>
+//         <br />
 //         <input
-//           onChange={(e) => setEmail(e.currentTarget.value)}
 //           type="text"
-//           value={email}
+//           value={this.state.email}
+//           onChange={this.handleInputChange}
+//           name="email"
 //         />
-//       </label>
-//       <br />
-//       <label>
-//         Password:
+//         <br />
+//         <label>Name: </label>
+//         <br />
 //         <input
-//           onChange={(e) => setPassword(e.currentTarget.value)}
 //           type="text"
-//           value={password}
+//           value={this.state.name}
+//           onChange={this.handleInputChange}
+//           name="name"
 //         />
-//       </label>
-//       <br />
-//       <button onClick={login}>Login</button>
-//       {isAuthenticated && <p>Login Successful!</p>}
-//     </div>
-//   );
+//         <br />
+//         <label>Password: </label>
+//         <br />
+//         <input
+//           type="text"
+//           value={this.state.password}
+//           onChange={this.handleInputChange}
+//           name="password"
+//         />
+//         <br />
+//         <button onClick={this.signup}>Signup </button>
+//       </>
+//     );
+//   }
+// }
+
+// const mapStateToProps = (state) => {
+//   return {
+//     isAuthticated: state.authStore.isAuthticated,
+//   };
+// };
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     signupMDP: (email, password, name) =>
+//       dispatch(signupThunk(email, password, name)),
+//   };
 // };
 
-// export default Login;
+// export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Signup));
+
+// Hooks:
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { signupThunk } from "../Redux/auth/actions";
+
+const Signup = (props) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+
+  const signup = () => {
+    dispatch(signupThunk(email, password));
+  };
+
+  return (
+    <div>
+      <label>
+        Username:
+        <input
+          onChange={(e) => setEmail(e.currentTarget.value)}
+          type="text"
+          value={email}
+        />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input
+          onChange={(e) => setPassword(e.currentTarget.value)}
+          type="text"
+          value={password}
+        />
+      </label>
+      <br />
+      <button onClick={signup}>Signup</button>
+    </div>
+  );
+};
+
+export default Signup;
