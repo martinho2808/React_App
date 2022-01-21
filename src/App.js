c4aa144cd9132c5136a36116f1004a00586978a8
Sam O'Shaughnessy <samoshaughnessy123@hotmail.com>
2022-01-21 14:42:26 +0800

    updating sass

--- src/App.js
+++ src/App.js
@@ -1,82 +1,82 @@
 // Hook based functional component  with an added React Element
-import logo from "./logo.svg";
-import "./App.scss";
-import { useState } from "react";
-import Element from "./Components/Element.js";
-import BootstrapElement from "./Components/BootstrapElement.js";
-import StatefulComponent from "./Components/StatefulComponent.js";
-import HoverableComponent from "./Components/HoverableStateComponent.js";
-import ShoppingList from "./Components/ShoppingList.js";
-import Loops from "./Components/Loops";
-import ClockNoLCM from "./Components/ClockNoLCM.js";
-import Counter from "./Components/Counter";
-import CurrencyConverter from "./Components/CurrencyConverter";
-import AddFormItem from "./Components/Form.js";
-import Clock from "./Components/Clock.js";
-import APICall from "./Components/APICall";
-import WhoIsInSpace from "./Components/WhoIsInSpace.js";
-import Top from "./Components/useEffectEx.js";
+import logo from './logo.svg'
+import './App.scss'
+import { useState } from 'react'
+import Element from './Components/Element.js'
+import BootstrapElement from './Components/BootstrapElement.js'
+import StatefulComponent from './Components/StatefulComponent.js'
+import HoverableComponent from './Components/HoverableStateComponent.js'
+import ShoppingList from './Components/ShoppingList.js'
+import Loops from './Components/Loops'
+import ClockNoLCM from './Components/ClockNoLCM.js'
+import Counter from './Components/Counter'
+import CurrencyConverter from './Components/CurrencyConverter'
+import AddFormItem from './Components/Form.js'
+import Clock from './Components/Clock.js'
+import APICall from './Components/APICall'
+import WhoIsInSpace from './Components/WhoIsInSpace.js'
+import Top from './Components/useEffectEx.js'
 
-export default function App(props) {
-  let [clock, setClock] = useState(false);
+export default function App (props) {
+  let [clock, setClock] = useState(false)
 
   // To be passed as props - Loops
   const array = [
-    { name: "Andy", age: 26, nationality: "Chinese" },
-    { name: "Bibek", age: 20, nationality: "Nepalese" },
-    { name: "Lesley", age: 26, nationality: "Hong Kong" },
-    { name: "Sam", age: 28, nationality: "British" },
-  ];
+    { name: 'Andy', age: 26, nationality: 'Chinese' },
+    { name: 'Bibek', age: 20, nationality: 'Nepalese' },
+    { name: 'Lesley', age: 26, nationality: 'Hong Kong' },
+    { name: 'Sam', age: 28, nationality: 'British' }
+  ]
 
   const shopping = [
-    { item: "Potato" },
-    { item: "Bacon" },
-    { item: "Mayonnaise" },
-  ];
+    { item: 'Potato' },
+    { item: 'Bacon' },
+    { item: 'Mayonnaise' }
+  ]
 
   return (
-    <div className="App">
-      <div className="test">does this work?</div>
-      <header className="App-header">
+    <div className='App'>
+      <div className='test'>does this work?</div>
+      <header className='App-header'>
         Hello world
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="btn btn-primary" // Added bootstrap with yarn add bootstrap, altered index.js
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='btn btn-primary' // Added bootstrap with yarn add bootstrap, altered index.js
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
       </header>
-      <Element age="99" name="Sam's World"></Element>
-      <BootstrapElement buttonLabel="Click me" />
+      <Element age='99' name="Sam's World"></Element>
+      <BootstrapElement buttonLabel='Click me' />
       <StatefulComponent />
       <HoverableComponent />
-      <ShoppingList list={shopping} name="Lesley" />
+      <ShoppingList list={shopping} name='Lesley' />
       <Loops />
       <button onClick={() => setClock(!clock)}>GetClock</button>
-      {clock ? <Clock /> : "No clock"}
+      {clock ? <Clock /> : 'No clock'}
       <Loops array={array} />
       <ClockNoLCM />
-      <APICall pokemon="pikachu" />
+      <APICall pokemon='pikachu' />
       <WhoIsInSpace />
-      <Counter name="Tom" />
-      <Counter name="William" />
-      <div className="row">
-        <div className="col-6">
+      <Counter name='Tom' />
+      <Counter name='William' />
+      <div className='row'>
+        <div className='col-6'>
           <AddFormItem />
         </div>
-        <div className="col-6">
+        <div className='col-6'>
           <CurrencyConverter />
         </div>
       </div>
       <Top />
     </div>
-  );
+  )
 }
 
 // // Class version of application with an added React Element
